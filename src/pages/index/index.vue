f76598a4e6b390d80f850cce4cb67ecb121e6546
wg233 <18851716394@qq.com>
2023-11-07 09:36:26 +0800

    安装 windicss & 封装 http.ts

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,8 +5,10 @@
 <template>
   <view class="index">index</view>
   <div class="ellipsis">哪里贵了? 这么多年都是这个价格</div>
+  <div class="flex justify-center h-100 items-center">
+    <div class="w-10 h-10 bg-pink-500"></div>
+  </div>
+  <div class="bg-yellow-200 w-730 h-100"></div>
 </template>
 
-<style lang="scss">
-//
-</style>
+<style lang="scss"></style>
