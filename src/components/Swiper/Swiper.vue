9301c2e8509901098a49a2600450fd081e2d94cd
wg-233 <1208361250@qq.com>
2023-11-20 09:46:52 +0800

    fix(轮播图): 修复轮播图指示点不会跟随切换的问题

--- src/components/Swiper/Swiper.vue
+++ src/components/Swiper/Swiper.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 const activeIndex = ref(0)
+// 当 swiper 下标发生变化时触发
+const onChange: UniHelper.SwiperOnChange = (ev) => {
+  activeIndex.value = ev.detail.current
+}
+// 定义 props 接收
 defineProps<{
   list: BannerItem[]
 }>()
